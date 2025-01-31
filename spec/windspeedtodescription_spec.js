f171a64c357a38771ad7ffc537add9ee330bd30a
abibi189 <145033589+abibi189@users.noreply.github.com>
2025-01-31 06:46:54 +0000

    add windspeedtodescription.js

--- spec/windspeedtodescription_spec.js
+++ spec/windspeedtodescription_spec.js
@@ -1,12 +1,18 @@
-import { convertWindSpeedToDescription } from '../windspeedtodescription.js'
+import { convertWindSpeedToDescription } from '../windspeedtodescription.js';
 
 describe("A suite", function() {
     it("should return 'Calm Breeze' for 5 knots", function() {
-      expect(convertWindSpeedToDescription(5)).toBe("Calm Breeze");
+        let wSpeed = convertWindSpeedToDescription(5);
+        expect(wSpeed).toBe("Calm Breeze");
     });
+
     it("should return 'Moderate Breeze' for 15 knots", function() {
-      expect(convertWindSpeedToDescription(15)).toBe("Moderate Breeze");
+        let wSpeed = convertWindSpeedToDescription(15);
+        expect(wSpeed).toBe("Moderate Breeze");
     });
-  it("should return 'Severe Gale' for 50 knots", function() {
-    expect(convertWindSpeedToDescription(50)).toBe("Severe Gale");
-  });
+
+    it("should return 'Severe Gale' for 50 knots", function() {
+        let wSpeed = convertWindSpeedToDescription(50); 
+        expect(wSpeed).toBe("Severe Gale");
+    });
+});
--- windspeedtodescription.js
+++ windspeedtodescription.js
@@ -1,13 +1,14 @@
-// 2) Convert a windspeed to  a description using The Beaufort Wind Scale.
-function convertWindSpeedToDescription((windSpeedKnots){
-    let description = "Calm Breeze";
+// 2) Convert a windspeed to a description using The Beaufort Wind Scale.
+function convertWindSpeedToDescription(windSpeedKnots) {
+    let description = "Calm Breeze"; // Default description
 
-    if (windSpeedKnots >= 41) {
+    if (windSpeedKnots > 40) {
         description = "Severe Gale";
-    }else if (windSpeedKnots >= 13){
+    } else if (windSpeedKnots > 13) {
         description = "Moderate Breeze";
     }
+
     return description;
 }
 
-export {convertWindSpeedToDescription};
+export { convertWindSpeedToDescription };
